27e586cbe8825fcb4ffb0959f6a285ad994f1b37
Henrique Magalhães <henrique.araujo@saint-gobain.com>
2021-05-24 21:44:55 -0300

    Missel inimigo explode ao te atingir

--- script-missel-boss.gd
+++ script-missel-boss.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
-
+var velocidade = 2 
 func _ready():
 	pass
 
 
 func _process(delta):
-	translate(Vector2(0,2))
+	translate(Vector2(0,velocidade))
 	
 	if ScriptGlobal.fim_de_jogo == true:
 		queue_free()
@@ -15,4 +15,12 @@ func _process(delta):
 func dano_no_marco(body):
 	if body.name == "KinematicBody2D":
 		ScriptGlobal.qtd_vidas += -5
+		velocidade = 0
 		print("Sofri dano ",ScriptGlobal.qtd_vidas)
+		$Area2D/CollisionShape2D.set_deferred("disable", true)
+		$Area2D/AnimationPlayer.play("explodindo")
+
+
+func explodiu(anim_name):
+	if anim_name == "explodindo":
+		queue_free() 
